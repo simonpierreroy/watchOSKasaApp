3f2c98775db7e6720c12675ec510e326a84056ed
Simon-Pierre Roy <simonpierreroy@gmail.com>
2023-07-16 11:38:13 -0400

    update

--- KasaCore/Package.swift
+++ KasaCore/Package.swift
@@ -23,7 +23,7 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/pointfreeco/swift-composable-architecture.git",
-            exact: "0.55.0"
+            exact: "0.55.1"
         ),
         .package(
             url: "https://github.com/pointfreeco/swift-tagged.git",
